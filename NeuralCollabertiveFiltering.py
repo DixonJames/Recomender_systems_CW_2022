04814507a83c23b0647d780478d690a223ada5c0
james.d <james@d2.com>
2022-01-01 20:48:23 +0000

    running training on DL model

--- NeuralCollabertiveFiltering.py
+++ NeuralCollabertiveFiltering.py
@@ -8,6 +8,9 @@ import numpy as np
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"device: {device}")
 
+# debug
+device = "cpu"
+
 
 def load(path):
     try:
@@ -18,20 +21,57 @@ def load(path):
         return None
 
 
+def getExampleData(item_vecs, user_latent_vecs):
+    item_v = np.array(item_vecs.iloc[[0]])[0]
+    user_v = user_latent_vecs[0]
+    return catVec(item_v, user_v)
+
+
+def catVec(item_v, user_v):
+    return torch.cat([torch.from_numpy(item_v).to(device), torch.from_numpy(user_v).to(device)])
+
+
+class MLPBlock(nn.Module):
+    def __init__(self, in_d, out_d):
+        super().__init__()
+        self.in_d = in_d
+        self.out_d = out_d
+
+        self.layer = nn.Sequential(nn.Linear(in_d, out_d), nn.ReLU())
+
+    def forward(self, x):
+        return self.layer(x)
+
 class MLP(nn.Module):
     def __init__(self, ex_input):
         super().__init__()
+        self.example = ex_input
+        self.input_dim = ex_input.shape
 
-    def block(self, in_dim, out_dim, x):
-        r = nn.Linear(in_dim, out_dim).forward(x)
-        return nn.ReLU().forward(r)
+        self.layer = nn.Sequential(nn.Flatten(),
+                                   MLPBlock(32, 16),
+                                   MLPBlock(32, 16),
+                                   MLPBlock(16, 8),
+                                   MLPBlock(8, 4),
+                                   nn.Linear(2, 1))
+
+    def block(self, in_dim, out_dim):
+        return nn.Sequential(nn.Linear(in_dim, out_dim), nn.ReLU())
+
+    def layers(self, x):
+        r = self.block(self.input_dim[0], 2 ** self.largestFactor(), x)
+        for p_i in len(self.pows):
+            r = self.block(self.pows[-p_i], self.pows[-(p_i + 1)], r)
+        return r
 
     def forward(self, x):
-        r = self.block(32 * 32 * 3, 64, x)
-        r = self.block(64, 32, r)
-        r = self.block(32, 16, r)
-        r = self.block(8, 4, r)
-        r = self.block(2, 1, r)
+        r = x.view(-1).float()
+        r = MLPBlock(self.input_dim[0], 32).forward(r)
+        r = MLPBlock(32, 16).forward(r)
+        r = MLPBlock(16, 8).forward(r)
+        r = MLPBlock(8, 4).forward(r)
+        r = MLPBlock(4, 2).forward(r)
+        r = MLPBlock(2, 1).forward(r)
 
         return r
 
@@ -47,7 +87,8 @@ class NuralCollab:
         self.user_n_to_index = self.interactions["userId"].unique()
         self.movie_n_to_index = self.interactions["movieId"].unique()
 
-        self.opt = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        self.betas = (0.5, 0.999)
+        self.opt = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate, betas=self.betas)
 
     def genInteractions(self, age=None, country=None, timeRange=None):
         """
@@ -58,36 +99,54 @@ class NuralCollab:
         :return:
         """
         for row in self.interactions.iterrows():
-            if row["age"] in range(age - 5, age + 5) or age is None:
-                if row["country"] == country or country is None:
+            if age is None:
+                if country is None:
                     # haevnbt implitmetd timerage yet
-                    user_i = np.where(self.user_n_to_index == row["userId"])[0]
-                    item_i = np.where(self.movie_n_to_index == row["movieId"])[0]
+                    user_i = np.where(self.user_n_to_index == row[1]["userId"])[0]
+                    item_i = np.where(self.movie_n_to_index == row[1]["movieId"])[0]
 
-                    user_latent_vec = self.user_latent_vecs[user_i]
-                    item_vec = self.item_vecs[item_i]
-                    rating = row["rating"]
+                    item_v = np.array(self.item_vecs.iloc[item_i])[0]
+                    user_v = self.user_latent_vecs[user_i][0]
 
-                    yield user_latent_vec, item_vec, rating
+                    input_v = catVec(item_v, user_v)
+                    rating = row[1]["rating"]
+
+                    yield input_v, rating
+
+    def constructvector(self, user_latent_vec, item_vec):
+        return catVec(item_vec, user_latent_vec)
 
     def lossFunc(self, prediction: float, rating: float) -> float:
         return prediction - rating
 
-    def train(self, epoch_num=10, ):
+    def exampleData(self):
+        row = self.interactions[0]
+        user_i = np.where(self.user_n_to_index == row["userId"])[0]
+        item_i = np.where(self.movie_n_to_index == row["movieId"])[0]
+
+        user_latent_vec = self.user_latent_vecs[user_i]
+        item_vec = self.item_vecs[item_i]
+
+        item_v = self.item_vec.iloc[[item_i]]
+        user_v = self.user_latent_vecs[user_i]
+
+        return self.constructvector(user_latent_vec, item_vec)
+
+    def train(self, epoch_num=10):
         for epoch in range(epoch_num):
             print(f"Epoch: {epoch}")
             batches = self.genInteractions()
             loss_arr = np.zeros(0)
 
             batch_number = 0
-            for user_latent_vec, item_vec, rating in batches:
+            for input_vec, rating in batches:
                 print(f"{batch_number}/500")
-                user_latent_vec, item_vec, rating = user_latent_vec.to(device), item_vec.to(device), rating.to(device)
+                rating = torch.from_numpy(np.array([rating]))
+                input_vec, rating = input_vec.to(device), rating.to(device)
 
-                query_vec = torch.cat(user_latent_vec, item_vec)
-
+                prediction = self.model(input_vec)
                 # increase loss if says real is fake
-                loss = self.lossFunc(rating)
+                loss = self.lossFunc(prediction, rating)
                 self.opt.zero_grad()
                 loss.backward(retain_graph=True)
                 self.opt.step()
@@ -102,7 +161,11 @@ class NuralCollab:
 
     def query(self, movieId, userId):
         with torch.no_grad():
-            pass
+            user_i = np.where(self.user_n_to_index == userId)[0]
+            item_i = np.where(self.movie_n_to_index == movieId)[0]
+
+            user_latent_vec = self.user_latent_vecs[user_i]
+            item_vec = self.item_vecs[item_i]
 
 
 def main(load_mat=True):
@@ -114,11 +177,16 @@ def main(load_mat=True):
         factoredMatrix = load("data/temp/factoredMatrix.obj")
     user_latent_vecs = factoredMatrix.user_latent_v
     item_latent_vecs = factoredMatrix.item_latent_v
-    user_interactions = UserVec(getCSV(ml_ratings)).user_df
+    user_interactions = getCSV(ml_ratings)
     item_data = ItemVec(None, None, None, None, load=True).clean_items
 
-    model = MLP(1)
-    NC = NuralCollab(NN_model=model, user_latent_vecs=user_latent_vecs, item_vecs=item_data, interactions=user_interactions)
+    exampleData = getExampleData(item_vecs=item_data, user_latent_vecs=user_latent_vecs)
+
+    model = MLP(exampleData).to(device)
+    NC = NuralCollab(NN_model=model, user_latent_vecs=user_latent_vecs, item_vecs=item_data,
+                     interactions=user_interactions)
+
+    NC.train()
 
 
 if __name__ == '__main__':
