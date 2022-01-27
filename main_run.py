ed489867e50d4fea71b31d5fc82f3e8ef64ddf56
james.d <james@d2.com>
2022-01-27 13:30:51 +0000

    started on UI

--- main_run.py
+++ main_run.py
@@ -1,7 +1,10 @@
+import sys
+
 from sklearn.metrics.pairwise import linear_kernel
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import numpy as np
+from datetime import datetime
 from scipy.stats.stats import pearsonr
 
 from data_cleaning import prepareData
@@ -11,10 +14,12 @@ from MatrixFactorisation import factoriseMatrix, MatrixFact
 from NeuralCollabertiveFiltering import neauralCollaberativeModel
 
 
+
+
 class User:
-    def __init__(self, whole_user_df, movie_df, user_id):
+    def __init__(self, whole_user_df, movie_df, user_id, MF_model=None, NCF_model=None):
         self.whole_user_df = whole_user_df
-        self.movie_df = movie_df
+        self.whole_movie_df = movie_df
         self.user_id = user_id
 
         self.user_df = whole_user_df.user_df[whole_user_df.user_df["userId"] == self.user_id]
@@ -24,6 +29,16 @@ class User:
             whole_user_df.user_df[whole_user_df.user_df["userId"] == self.user_id]["mean_score"])
 
         self.movie_df = movie_df.clean_items
+
+        if MF_model is not None:
+            self.MF_model = MF_model
+        else:
+            self.MF_model = factoriseMatrix(load_matrix=False, ratings=self.whole_user_df.ratings)
+
+        if NCF_model is not None:
+            self.NCF_model = NCF_model
+        else:
+            self.NCF_model = neauralCollaberativeModel(load_model=False, ratings=self.whole_user_df.ratings)
 
     def ratingWeighting(self, rating):
         """
@@ -46,7 +61,7 @@ class User:
         for index, row in self.user_reviews.iterrows():
             rating = row["rating"]
             movie_id = row["movieId"]
-            movie_vec = np.array(self.movie_df.iloc[int(movie_id)])
+            movie_vec = np.array(self.movie_df.loc[int(movie_id)])
             weighting = self.ratingWeighting(rating)
 
             user_vec += (weighting * movie_vec)
@@ -62,7 +77,10 @@ class User:
         for index, row in self.user_reviews.iterrows():
             rating = int(row["rating"])
             movie_id = row["movieId"]
-            movie_vec = np.array(self.movie_df.iloc[int(movie_id)])
+            try:
+                movie_vec = np.array(self.movie_df.loc[int(movie_id)])
+            except:
+                pass
 
             user_vec_classes[rating - 1] += movie_vec
 
@@ -72,23 +90,142 @@ class User:
         return user_vec_classes, user_weight_classes
 
     def contentBasedPrediction(self):
-        content_based = ContentCompare(self.movie_df, self)
-        all_predictions = content_based.queryUser(self.user_id, profile_vector_type="classed",
-                                                  distance_measure="cosine")
+        content_based = ContentCompare(self.whole_movie_df, self)
+        all_predictions = content_based.queryUser(profile_vector_type="weighted", distance_measure="cosine")
         return all_predictions
 
     def matrixFactorPrediction(self):
-        mat = factoriseMatrix(load=False, ratings=self.whole_user_df.ratings)
-        all_predictions = mat.allPredictions(self.user_id)
+        all_predictions = self.MF_model.allPredictions(self.user_id)
         return all_predictions
 
     def NCFPPrediction(self):
-        NCM = neauralCollaberativeModel(load_mat=True, load_model=False, ratings=self.whole_user_df.ratings)
-        all_predictions = NCM.allPredictions(self.user_id)
+        all_predictions = self.NCF_model.allPredictions(self.user_id)
         return all_predictions
 
 
+class RecommenderSystem:
+    def __init__(self):
+        self.items, self.users = prepareData(load_stored_data=True, reduce=True, min_user_reviews=100,
+                                             min_movie_raings=50)
+
+        self.current_user = None
+
+    def startUp(self):
+        """
+        first thign to run
+        inincal user login
+        runs main menue for first time
+        :return:
+        """
+        print("======== Welcome ==========")
+        self.selectUser()
+        self.mainMenue()
+
+    def mainMenue(self):
+        print("===========================")
+        print("========== Menu ===========")
+        print("===========================")
+
+        print("1)   select user login")
+        print("2)   get recommendations")
+        print("3)   crete new rating")
+        print("4)   quit session")
+
+        option = self.IOResponse("Enter selection", [1, 2, 3, 4])
+        if option == 1:
+            self.selectUser()
+        elif option == 2:
+            self.generateRecomendations()
+        elif option == 3:
+            self.createRating()
+        elif option == 4:
+            sys.exit()
+
+    def IOResponse(self, message, options=None):
+
+        response = "_"
+        if options is not None:
+            options = [str(i) for i in options]
+            print(f"Valid inputs: {options}")
+            while response not in options:
+                response = input(f"{message}:")
+        else:
+            response = input(f"{message}:")
+
+        print()
+        return response
+
+    def createRating(self):
+        movieId = "N"
+        i = 0
+        while movieId != "N":
+            disp_movies = self.items.genres["movieId"].unique()[:5 + i]
+            print(disp_movies)
+            print("enter 'N' for next set of movies ")
+            movieId = self.IOResponse("please select existing user ID", disp_movies.append("N"))
+            i += 1
+
+        # create rating
+        rating = self.IOResponse("please enter a movie rating", [1, 2, 3, 4, 5])
+        timestamp = datetime.datetime(2022, 30, 1).timestamp()
+
+        self.users.ratings.append({"userId": self.current_user.user_id})
+
+    def warmUpRatings(self):
+        """
+        gets a new user to create some warm up ratings
+        :return:
+        """
+        print("===== Rating 5 movies =====")
+        for i in range(5):
+            self.createRating()
+
+    def createNewUser(self):
+        """
+        adds new user to dataframes
+        returns new users ID
+        """
+        new_id = max(self.users.user_df["userId"].unique()) + 1
+
+    def selectUser(self):
+        """
+        selects the current user.
+        select pre_existing or create new one
+        """
+        print("===== SELECTING USER =====")
+        NEW_USER_OPTION = self.IOResponse("Select: new(N) or existing(E) user", ["N", "n", "E", "e"])
+
+        if NEW_USER_OPTION.upper() == "N":
+            userId = self.createNewUser()
+
+        else:
+            # display potencail users
+            selected = False
+            i = 1
+            while not selected:
+                disp_users = list(self.users.user_df["userId"].unique()[5*(i-1):5 * i])
+                print("enter 'next' for next set of user IDs")
+                disp_users.append("next")
+                userId = self.IOResponse("please select existing user ID", disp_users)
+                if userId == "next":
+                    i += 1
+                else:
+                    selected = True
+        userId = int(userId)
+
+        self.current_user = User(whole_user_df=self.users, movie_df=self.items, user_id=userId)
+
+    def generateRecomendations(self):
+        """
+        select desired recommender system
+        churns out as many responses as required
+        :return:
+        """
+        pass
+
+
 if __name__ == '__main__':
-    items, users = prepareData(load_stored_data=True, reduce=True, min_user_reviews=100, min_movie_raings=50)
-    test_u = User(whole_user_df=users, movie_df=items, user_id=1)
-    test_u.NCFPPrediction()
+    # items, users = prepareData(load_stored_data=True, reduce=True, min_user_reviews=100, min_movie_raings=50)
+    # test_u = User(whole_user_df=users, movie_df=items, user_id=1)
+    rs = RecommenderSystem()
+    rs.startUp()
